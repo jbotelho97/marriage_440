b7b09f3f4b3a9f558366e6b5cf161f11a1a4788d
Jack A. Botelho <35936122+jbotelho97@users.noreply.github.com>
2019-02-04 14:09:52 -0500

    Add files via upload
    Current working version, still need to fix file opening

--- marriage.py
+++ marriage.py
@@ -1,25 +1,23 @@
-#Authors: Jack Botelho, ...
-#Programmed in PycharmCE on a Macbook with a 1.4GHz i5
-#CSC 440 - Assignment 1
+# Authors: Jack Botelho, ...
+# Programmed in PycharmCE on a Macbook with a 1.4GHz i5
+# CSC 440 - Assignment 1
 
 import sys
 
-def main(argv):
-    # people = open(argv, "r")
+
+def main():
     try:
-        people = open(argv, "r")
-        num = int(people.readline())
+        people = open(sys.argv[1], "r")
     except:
         exit(1)
-    # num = int(people.readline())
+    num = int(people.readline())
     listNames = num + 1
     knights = [[0 for x in range(num)] for y in range(listNames)]
     ladies = [[0 for x in range(num)] for y in range(listNames)]
-    #pairs = [[0 for x in range(num)] for y in range(num)]
     pairs = []
     curLine = people.readline()
-    kcounter = 1  #knight counter
-    lcounter = 1  #lady coutner
+    kcounter = 1  # knight counter
+    lcounter = 1  # lady coutner
     while curLine != "":
         curLine = curLine.strip()
         if kcounter <= num:
@@ -28,38 +26,49 @@ def main(argv):
         else:
             ladies[lcounter - 1] = curLine.split(" ")
             lcounter += 1
-
         curLine = people.readline()
-    currentPick = 1
     pairCounter = 0
+
+    # Main pairing loop, while there are less then n pairs the loopw will continue
     while pairCounter < num:
-        print('I made it!')
-        for i in range(len(knights)):
-            if free(pairs, knights[i][0], 0) <= 0:
-                lady = knights[i][currentPick]
-                for j in range(len(ladies)):
+        for i in range(len(knights) - 1):  # loops through knights
+            if free(pairs, knights[i][0], 0) < 0:
+                lady = knights[i][1]
+                for j in range(len(ladies) - 1):
                     if lady == ladies[j][0]:
                         ladyPair = free(pairs, lady, 1)
-                        if ladyPair <= 0:
-                            pairs.append([i, j])
+                        if ladyPair < 0:
+                            pairs.append([knights[i][0], ladies[j][0]])
+                            knights[i].pop(1)
                             pairCounter += 1
                         else:
-                            knightPos #The position of the requestor in lady's list
+                            knightPos = 0  # The position of the requestor in lady's list
                             for k in range(len(ladies[j])):
                                 if knights[i][0] == ladies[j][k]:
                                     knightPos = k
                                     break
-                            if knightPos < pairs[ladyPair][0]:
-                                pairs.remove(ladyPair);
-                                pairs.append([i, j])
-                                pairCounter += 1
+                            # if knightPos < pairs[ladyPair][0]:
+                            comp = pairs[ladyPair][0]
+                            for m in range(num + 1):
+                                if comp == ladies[j][m]:
+                                    if knightPos < m:
+                                        pairs.pop(ladyPair);
+                                        pairs.append([knights[i][0], ladies[j][0]])
+                                        knights[i].pop(1)
+                                        for i in range(len(knights)):
+                                            if m == knights[i][0]:
+                                                knights[i].pop(1)
+                                                break
+                                    else:
+                                        knights[i].pop(1)
+                                        break
     for i in range(len(pairs)):
         dude, lady = pairs[i][0], pairs[i][1]
         print(dude, " ", lady)
+    people.close()
 
 
-
-#num is 0 for knights, 1 for ladies
+# num is 0 for knights, 1 for ladies
 def free(pairs, name, num):
     if num < 0 or num > 1:
         return -1
@@ -67,10 +76,3 @@ def free(pairs, name, num):
         if name == pairs[i][num]:
             return i
     return -1
-
-
-
-
-
-
-
